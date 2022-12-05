4b5bfe3cec0e39f2c643c6ee9f26b843c97b7953
awjin <awjinahn@gmail.com>
2022-12-05 03:05:15 -0800

    Allow preserving whitespace on inputs.
    Ugh - pretty ugly code.

--- utils/input.ts
+++ utils/input.ts
@@ -3,19 +3,25 @@ import { readFileSync } from "fs";
 export function read(
   year: number,
   day: number,
-  options?: { preserveNewlines: boolean }
+  options?: {
+    preserveSpaces?: boolean;
+    preserveNewlines?: boolean;
+  }
 ): string[][] {
-  const input = process.env["test"] ? "test" : "input";
-  return readFileSync(`${year}/${dayString(day)}/${input}`)
-    .toString()
-    .trim()
-    .split(options?.preserveNewlines ? /\n/ : /\n+/)
-    .map((line: string) =>
-      line
-        .trim()
-        .split(/\s+/)
-        .map((value: string) => (value === "" ? "\n" : value))
-    );
+  let file = readFileSync(
+    `${year}/${dayString(day)}/${process.env["test"] ? "test" : "input"}`
+  ).toString();
+
+  if (!options?.preserveSpaces) {
+    file = file.trim();
+  }
+
+  const lines = file.split(options?.preserveNewlines ? /\n/ : /\n+/);
+
+  return lines.map((x: string) => {
+    const line = options?.preserveSpaces ? [x] : x.trim().split(/\s+/);
+    return line.map((value: string) => (value === "" ? "\n" : value));
+  });
 }
 
 export function dayString(day?: number | string): string | undefined {
